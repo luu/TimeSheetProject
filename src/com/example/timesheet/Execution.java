13f3af8e0c134be17739bfec827428f1b825d7b8
Luu Hong Tu <hongtu.luu@gmail.com>
2013-03-16 23:33:39 +0100

    travail sur la base de donnée
    - ajout des get et set dans Execution
    - changer le type de datestart et dateEnd
    
    Signed-off-by: Luu Hong Tu <hongtu.luu@gmail.com>

--- src/com/example/timesheet/Execution.java
+++ src/com/example/timesheet/Execution.java
@@ -6,12 +6,51 @@ import android.text.format.DateFormat;
 
 public class Execution {
 	protected int id;
-	protected DateFormat dateStart;
-	protected DateFormat dateEnd;
+	protected Date dateStart;
+	protected Date dateEnd;
 	protected long duration;
 	protected String comments;
 	
 	public Execution(int id){
-		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getDateStart() {
+		return dateStart;
+	}
+
+	public void setDateStart(Date dateStart) {
+		this.dateStart = dateStart;
+	}
+
+	public Date getDateEnd() {
+		return dateEnd;
+	}
+
+	public void setDateEnd(Date dateEnd) {
+		this.dateEnd = dateEnd;
+	}
+
+	public long getDuration() {
+		return duration;
+	}
+
+	public void setDuration(long duration) {
+		this.duration = duration;
+	}
+
+	public String getComments() {
+		return comments;
+	}
+
+	public void setComments(String comments) {
+		this.comments = comments;
 	}
 }
